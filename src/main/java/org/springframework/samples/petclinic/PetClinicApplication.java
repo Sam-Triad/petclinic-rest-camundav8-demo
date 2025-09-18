2088b773d436d5ea0338abcde52bc68266c12514
Sam-Douglas <sdouglas.contact@gmail.com>
2025-09-18 11:34:31 +0100

    Automated deployments

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+
 @SpringBootApplication
+@Deployment(resources = {
+	"classpath*:/processes/**/*.bpmn",
+	"classpath*:/form/**/*.form"
+})
 public class PetClinicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
